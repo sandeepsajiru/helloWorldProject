39f899b75d1bd2ea3a4cf4e5eb734dbfadb5e7e0
unknown <kotihemant@gmail.com>
2015-06-24 02:54:52 -0700

    Person class

---
+++ Person.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Person{
+public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		String name  = scan.next();
+		int age = scan.nextInt();
+		System.out.println("The Person's name is : "+name+ " and age is : "+age);
+	}
+}
