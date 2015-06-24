0ac716df0319331aab615dcd50ea765fb39604cf
unknown <bhavanilakkarusu@gmail.com>
2015-06-24 15:24:25 -0700

    Bhavani first checkin

---
+++ address.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+public class address{
+    String city;
+    String House;
+    String pincode;
+    public static void main(string args[])
+{     
+    Scanner x=new Scanner(System.in);
+    System.out.println("enter city");
+    city=x.next();
+System.out.println("enter House");
+    House=x.next();
+System.out.println("enter pincode");
+    pincode=x.next();
+
+
+}
+}
+   
+
+
